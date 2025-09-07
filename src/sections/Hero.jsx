fbc35fb81622b566d31f8d2b933aaf50f8b4d7bf
eszain <zain36021@outlook.com>
2025-09-07 15:17:46 -0400

    Fix: Correct case sensitivity for ParallaxBackground and Frameworks imports

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,6 @@
 import { Canvas, useFrame } from "@react-three/fiber";
 import HeroText from "../components/HeroText";
-import ParallaxBackground from "../components/ParallaxBackground";
+import ParallaxBackground from "../components/parallaxBackground";
 import { Phoenix } from "../components/Phoenix";
 import { Float } from "@react-three/drei";
 import { useMediaQuery } from "react-responsive";
